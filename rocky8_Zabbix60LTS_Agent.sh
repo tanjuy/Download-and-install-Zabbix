ab6a58324d9a6d3897279a04f0d50839992d9136
Tanju <tanjuyucal@gmail.com>
2023-10-29 20:02:15 +0300

    Zabbix Agent edited

--- rocky8_Zabbix60LTS_Agent.sh
+++ rocky8_Zabbix60LTS_Agent.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Variables:
+IP_address=$(ip -c a | grep 'inet ' | grep -v '127' | awk '{print $2}' | awk -F '/' '{print $1}')
+
 echo 'Installing Zabbix repository'
 sleep 1
 rpm -Uvh https://repo.zabbix.com/zabbix/6.0/rhel/8/x86_64/zabbix-release-6.0-4.el8.noarch.rpm
@@ -23,15 +26,35 @@ zabbix_agentd_conf=/etc/zabbix/zabbix_agentd.conf
 
 read -p 'Enter a Zabbix Server IP: ' zabbix_server_ip
 
-sed -i "s/^Server=/Server=$zabbix_server_ip/" $zabbix_agentd_conf
+if grep -q 'Server=127.0.0.1' $zabbix_agentd_conf
+	sed -i "s/^Server=127.0.0.1/Server=$zabbix_server_ip/" $zabbix_agentd_conf
+	sed -i "s/^ServerActive=127.0.0.1/ServerActive=$zabbix_server_ip/" $zabbix_agentd_conf
+	sed -i "s/^Hostname=Zabbix server/Hostname=$(hostname)/" $zabbix_agentd_conf
+else
+	echo "Please, configure manually $zabbix_agentd_conf file"
+	echo "Edit Server=$IP_address and ServerActive=$IP_address and Hostname=$(hostname)"	
+fi
 
-sed -i "s/^ServerActive=/ServerActive=$zabbix_server_ip/" $zabbix_agentd_conf
 
-hostname_var=$(hostname)
-
-sed -i "s/^Hostname=Zabbix Server/Hostname=$hostname_var/" $zabbix_agentd_conf
+echo "Firewall 10050/tcp port added"
+firewall-cmd --add-port=10050 --permanent
+firewall-cmd --reload
 
 echo 'Restrating zabbix-agent'
 sleep 2
 systemctl restart zabbix-agent
+
+
+
+printf "
+	Zabbix Server Browser ---> Configuration Section ---> Hosts Subsection ---> Create host
+	Popup:
+		Host name: $(hostname)
+		Visible name: 
+		Templates: 
+		Groups: 
+		Add (click for adding interface)
+			--> Select Agent or SNMP or JMX or IPMI
+				--> Enter Zabbix Agent $IP_address	
+"
 
